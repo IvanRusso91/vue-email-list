d4d5afcf683334d09ff09f69afd1e7a209dd68c8
IvanRusso91 <iv.russo91@gmail.com>
2022-05-11 18:14:43 +0200

    aggiunti effetti e abbellimenti

--- js/script.js
+++ js/script.js
@@ -18,7 +18,10 @@ const app= new Vue({
       for(let i = 0; i < 10 ; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( res => {
           this.emails.push( res.data.response );
-          this.isLoading = false;
+          setTimeout(()=>{
+            this.isLoading = false;
+          },3000)
+          
         })  
       }
     },
